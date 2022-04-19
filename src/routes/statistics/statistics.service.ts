2e37dc7cdea82b89079d23389e63ea501be38e4c
Best-candidate <reifeisr@gmail.com>
2022-04-19 14:00:54 +0300

    implementation of fetching top 10 creators

--- src/routes/statistics/statistics.service.ts
+++ src/routes/statistics/statistics.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Get, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Creator } from 'src/schemas/creator.schema';
@@ -7,4 +7,9 @@ import { Creator } from 'src/schemas/creator.schema';
 @Injectable()
 export class StatisticsService {
     constructor (@InjectModel('Creator') private creatorModel: Model<Creator>) {}
+
+    async getTopCreators() {
+        return await this.creatorModel.find().sort({postNumber: -1}).limit(10)
+    }
+
 }
