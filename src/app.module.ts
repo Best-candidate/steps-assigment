25c1c8abcdf4158fe6a2758d6401fbf1836a9aba
Best-candidate <reifeisr@gmail.com>
2022-04-19 10:54:56 +0300

    mongo connection implemented

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.g5nob.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)],
   controllers: [AppController],
   providers: [AppService],
 })
